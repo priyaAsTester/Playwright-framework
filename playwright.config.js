ae59753e27f174fbe224feb742fd92fa7b5b41fe
unknown <sabaripriya@cloudrevelinnovation.com>
2026-04-17 15:34:59 +0530

    allure reports

--- playwright.config.js
+++ playwright.config.js
@@ -12,12 +12,14 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
 
-  reporter: 'html',
-
+  reporter:[ ['html'],  ['allure-playwright']],
   use: {
     baseURL: process.env.BASE_URL,
     headless: true,
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+
   },
 
   projects: [
--- tests/logout.spec.js
+++ tests/logout.spec.js
@@ -1,12 +1,21 @@
 const { test, expect } = require('../fixtures/baseTest');
 
-test('@ regressionverifying Logout', async ({ loggedInPage }) => {
+test('@regressionverifying Logout', async ({ loggedInPage }) => {
   // loggedInPage ensures the user is authenticated and can run independently
   // The test automatically logs in before running, ensuring reliable auth
-  
+  await test.step('checking url before logout',async()=>{
   await expect(loggedInPage.page).toHaveURL(/.*\/secure/);
+  page.screenshot({path:'screenshot/image1.png'})
+  })
 
+await test.step('clicking logout',async()=>{
   await loggedInPage.dashboardPage.Logout();
+   page.screenshot({path:'screenshot/image2.png'})
+})
 
-  await expect(loggedInPage.page).toHaveURL(/.*\/login/);
+await test.step('after logout',async()=>{
+ await expect(loggedInPage.page).toHaveURL(/.*\/login/);
+  page.screenshot({path:'screenshot/image3.png'})
+})
+ 
 });
